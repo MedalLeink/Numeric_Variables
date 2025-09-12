801b3843d873c3214335fe23faf5b0375d930d3b
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-09-12 12:53:45 -0400

    variables in python

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# to determine the length of a song
+
+song1 = 421
+song2 = 300
+song3 = input("What is the length of the song you wanna upload?: ")
+
+song3_input = int(song3)
+
+playlist = song1 + song2 + song3_input
+
+print(f"The length of the song you wanna upload is {playlist} seconds.")
