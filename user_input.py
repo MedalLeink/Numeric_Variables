687e78cc68a7da7f3ef12cb75940e3eea827fd9a
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-09-12 13:36:04 -0400

    float used instaed of int

--- user_input.py
+++ user_input.py
@@ -2,10 +2,11 @@
 
 song1 = 421
 song2 = 300
-song3 = input("What is the length of the song you wanna upload?: ")
+# song3 = input("What is the length of the song you wanna upload?: ")
+song3 = float(input("What is the length of the song you wanna upload?: "))
 
-song3_input = int(song3)
+# song3_input = int(song3)
 
-playlist = song1 + song2 + song3_input
+playlist = song1 + song2 + song3
 
 print(f"The length of the song you wanna upload is {playlist} seconds.")
