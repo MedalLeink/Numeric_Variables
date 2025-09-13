f217bc11c6a10315d095ef6cda8ff4612a72b1e2
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-09-13 14:16:32 -0400

    program that prompts user for his/her age

--- user_input.py
+++ user_input.py
@@ -1,12 +1,35 @@
-# to determine the length of a song
+# # to determine the length of a song
 
-song1 = 421
-song2 = 300
-# song3 = input("What is the length of the song you wanna upload?: ")
-song3 = float(input("What is the length of the song you wanna upload?: "))
+# song1 = 421
+# song2 = 300
+# # song3 = input("What is the length of the song you wanna upload?: ")
+# song3 = float(input("What is the length of the song you wanna upload?: "))
 
-# song3_input = int(song3)
+# # song3_input = int(song3)
 
-playlist = song1 + song2 + song3
+# playlist = song1 + song2 + song3
 
-print(f"The length of the song you wanna upload is {playlist} seconds.")
+# print(f"The length of the song you wanna upload is {playlist} seconds.")
+
+
+
+
+# x = 12
+# y = 5
+# z = 2
+# result = (x+y) // z
+
+# print(result)
+
+
+
+
+"""
+Program that prompts the user for their age
+"""
+
+age = int(input("Let us know your age: "))
+age_addition = age + 1
+
+# print(f"Awwnn! You're still much young and vibrant! You'll be {age_addition} years old on your next birthday!🥳")
+print(f"On your next birthday, you will be {age_addition} years old!🥳")
