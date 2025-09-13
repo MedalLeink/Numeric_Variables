612d01830e2949904a151e2df8c7660dffd9f732
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-09-13 14:33:04 -0400

    Program that prompts the user for a number of cookies and a number of people

--- user_input.py
+++ user_input.py
@@ -29,19 +29,36 @@
 # """
 
 # age = int(input("Let us know your age: "))
-# age_addition = age + 1
+# # age_addition = age + 1
 
 # # print(f"Awwnn! You're still much young and vibrant! You'll be {age_addition} years old on your next birthday!🥳")
-# print(f"On your next birthday, you will be {age_addition} years old!🥳")
+# # print(f"On your next birthday, you will be {age_addition} years old!🥳")
+# print(f"On your next birthday, you will be {age_addition + 1} years old!🥳")
+
+
+
+
+# """
+# Program that prompts the users for the number of egg cartons
+# """
+
+# egg_carton = int(input("how many egg cartons do you have?: "))
+# egg_carton_multiplication = egg_carton * 12
+
+# print(f"You have {egg_carton_multiplication} eggs.")
 
 
 
 
 """
-Program that prompts the users for the number of egg cartons
+Program that prompts the user for a number of cookies and a number of people
 """
 
-egg_carton = int(input("how many egg cartons do you have?: "))
-egg_carton_multiplication = egg_carton * 12
+cookies = int(input("\nHow many cookies do you have?: "))
+people = int(input("How many people are there?: "))
 
-print(f"You have {egg_carton_multiplication} eggs.")
+cookies_total = float(cookies/people)
+# cookies_total = int(cookies/people)
+
+print(f"Each person may have {cookies_total} cookies")
+
